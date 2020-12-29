c6162975e39bfe4764dc36bf7a421e5c4aadac99
giladrozmarin <19gilad@gmail.com>
2020-12-29 20:32:29 +0200

    authCredit function

--- midProject/model/authentication.js
+++ midProject/model/authentication.js
@@ -1,8 +1,24 @@
 const usersDAL = require('../DAL/usersDAL')
+const userManagement = require('../model/userManagement')
 
 exports.authenticationUser = async (userName,password) => {
     //get data user list
     let data = await usersDAL.getUsers();
     return user = data.users.filter(x => (x.Username === userName) && (x.Password ===password))
+
+}
+exports.authCredit = (req) => {
+
+     if(req.session.transactions > 0 || req.session.transactions === true){
+        if(typeof req.session.transactions !== "boolean")
+        {
+            console.log(req.session.transactions)
+         req.session.transactions = req.session.transactions-1
+         userManagement.updateTransactions(req.session.user)
+        }
+        return true
+     }
+     else return false
+
 
 }
