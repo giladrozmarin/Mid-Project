88698b35d8b26d2aae265183380f8e8ad13106c6
giladrozmarin <19gilad@gmail.com>
2020-12-29 20:32:55 +0200

    updateTransactions function

--- midProject/model/userManagement.js
+++ midProject/model/userManagement.js
@@ -38,11 +38,56 @@ exports.updateUser = async(obj) =>
         data.users[index].Password = obj.Password
         data.users[index].CreatedDate = obj.CreatedDate
         data.users[index].NumOfTransactions = obj.NumOfTransactions
+        data.users[index].lastLogin =  data.users[index].lastLogin
+        data.users[index].dailyTransactions =  data.users[index].dailyTransactions
     }
     else
     {
+        let source = {
+            ["lastLogin"]:"",
+            ["dailyTransactions"]:""
+        }
+        const returnedTarget = Object.assign(obj, source);
+           console.log(returnedTarget)
         data.users.push(obj)
     }
     usersDAL.setUsers(data.users)
   
 }
+exports.updateTransactions = async(Username) =>
+{
+    let data = await usersDAL.getUsers()
+    let index = -1
+    data.users.map((v,i) => {if(v.Username === Username)  index=i } )
+   
+
+
+        data.users[index].dailyTransactions = data.users[index].dailyTransactions-1
+    
+
+    
+    
+    console.log(data.users)
+    usersDAL.setUsers(data.users)
+  
+}
+exports.loadCredit = async(Username) =>
+{
+    let data = await usersDAL.getUsers()
+    let index = -1
+    data.users.map((v,i) => {if(v.Username === Username.Username)  index=i } )
+    console.log(index)
+    
+      
+        data.users[index].lastLogin =  Username.lastLogin
+        data.users[index].dailyTransactions = Username.dailyTransactions 
+        console.log(data.users)
+        usersDAL.setUsers(data.users)
+    }
+
+       
+    
+ 
+  
+
+
